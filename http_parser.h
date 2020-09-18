73cea6f0487f6f58b076098abfad7ea5de60d4d6
CSharperMantle <mantlejonse@gmail.com>
2020-09-19 07:18:25 +0800

    edit keywords.txt and copyright info

--- http_parser.h
+++ http_parser.h
@@ -1,4 +1,6 @@
-/* Copyright Joyent, Inc. and other Node contributors. All rights reserved.
+/* Copyright Joyent, Inc. and other Node contributors.
+ * Copyright Rong "Mantle" Bao (aka CSharperMantle).
+ * All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
